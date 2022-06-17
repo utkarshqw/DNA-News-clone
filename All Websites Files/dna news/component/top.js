a338b8e5b93c9b41decc9af2f5beae47f781d308
utkarshqw <utkarshup75@gmail.com>
2022-06-17 17:48:31 +0530

    Our Website

---
+++ All Websites Files/dna news/component/top.js
@@ -0,0 +1,12 @@
+function top() {
+    return `
+    <div><p>Top News</p></div>
+    <div id="slideshow">
+    </div>
+    <div id="play">
+     <i class="fa-solid fa-backward-step" ></i>
+     <i class="fa-solid fa-pause"></i>
+     <i class="fa-solid fa-forward-step"></i>
+    </div>`;
+}
+export default top;
